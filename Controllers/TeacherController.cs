aea91c475db52cfeec51570e0f508266d7b291cd
Sayan0361 <sayansen0361@gmail.com>
2026-02-20 14:28:14 +0530

    Add Teacher Specialization on Teacher/Onboarding/{index} : WIP frontend

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,17 +1,55 @@
-﻿using System;
+﻿using Dapper;
+using PrepMaster.DAL;
+using PrepMaster.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace PrepMaster.Controllers
 {
     public class TeacherController : Controller
     {
+        private readonly TeacherDAL _dal;
+        public TeacherController()
+        {
+            _dal = new TeacherDAL();
+        }
         // GET: Teacher
         public ActionResult Index()
         {
             return View();
+        }
+
+        // Onboard the Teacher : Teacher must fulfill the details
+        // Details : Select Subjects and Classes
+        public ActionResult Onboarding(int id)
+        {
+            var availableSubjectsAndClasses = _dal.GetSubjectsAndClasses();
+            var viewmodel = new TeacherOnboardingVM
+            {
+                TeacherId = id,
+                AvailableSubjects = availableSubjectsAndClasses
+            };
+
+            return View(viewmodel);
+        }
+
+        // Post req : for adding TeacherSpealization
+        // TeacherId and MatchIdList will come from Jquery ajax call
+        // MatchIdList = "1,2,3,4"
+        [HttpPost]
+        public ActionResult AddSpecialization(int TeacherId, string MatchIdList)
+        {
+            var result = _dal.AddTeacherSpecialization(TeacherId, MatchIdList);
+
+            return Json(new
+            {
+                success = result.Success == 1,
+                message = result.Message
+            });
         }
     }
 }
---
+++ Models/TeacherModel.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using PrepMaster.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PrepMaster.Models
+{
+    public class Teacher
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public int Role { get; set; }
+    }
+    public class GetSubjectsAndClassesVM
+    {
+        public int MatchId { get; set; }
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+    }
+    public class TeacherOnboardingVM
+    {
+        public int TeacherId { get; set; }
+        public List<GetSubjectsAndClassesVM> AvailableSubjects { get; set; }
+    }
+    public class DbResponse
+    {
+        public int Success { get; set; }
+        public string Message { get; set; }
+    }
+}
+
+namespace PrepMaster.DAL
+{
+    public class TeacherDAL
+    {
+        private readonly DapperConn _conn;
+        public TeacherDAL()
+        {
+            _conn = new DapperConn();
+        }
+
+        public List<GetSubjectsAndClassesVM> GetSubjectsAndClasses()
+        {
+            var proc = "sp_GetSubjectsAndClasses";
+            return _conn.ExecuteMultipleRow<GetSubjectsAndClassesVM>(proc);
+        }
+
+        public DbResponse AddTeacherSpecialization(int TeacherId, string MatchIdList)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@TeacherId", TeacherId);
+            parameters.Add("@MatchIdList", MatchIdList);
+
+            var proc = "sp_AddTeacherSpecialization";
+
+            return _conn.ExecuteSingleRow<DbResponse>(
+                proc,
+                parameters
+            );
+        }
+    }
+}
